ae126ae9ec7c6e35c22385993ebbfdb7f39e941a
valeriareynososf <valeriayearup@gmail.com>
2021-09-21 06:03:46 -0700

    saving

--- routes/questions.js
+++ routes/questions.js
@@ -2,16 +2,28 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const db = require('../db/models');
 const { asyncHandler } = require('./utils');
-
+const { Question } = db;
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req,res) => {
-    const users = await db.User.findAll({
-        attributes: ['userName']
-    });
-    //res.json({ users });
-    res.send("hi")
+    const questions = await db.Question.findAll();
+    res.render("questions", { questions })
 }));
+
+router.get("/questions/:id(\\d+)", asyncHandler(async (req, res) => {
+    //const questionId = parseInt(req.params.id, 10);
+    const questions = await Question.findByPk(req.params.id);
+    //res.render("questions", { questions });
+    //res.json({ questions });
+    res.send("hello");
+  })
+);
+
+router.get("/questions/hello",
+  asyncHandler(async (req, res) => {
+    res.send("hello");
+  })
+);
 
 
 module.exports = router;
