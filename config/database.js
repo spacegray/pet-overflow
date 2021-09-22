4fcc0363e3fcf26931c31d249b2b253687163567
valeriareynososf <valeriayearup@gmail.com>
2021-09-22 12:38:54 -0700

    deploying to heroku

--- config/database.js
+++ config/database.js
@@ -7,11 +7,16 @@ const database = db.database;
 const host = db.host;
 
 module.exports = {
-    development: {
-        username,
-        password,
-        database,
-        host,
-        dialect: 'postgres',
-    },
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: "postgres",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
 };
