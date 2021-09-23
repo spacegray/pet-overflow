dffc1ded8ceab388d8ae726477d399ee586743c4
9ziggy9 <davidarogers@protonmail.com>
2021-09-22 22:52:23 -0700

    database fixed, column added; heroku migration should be as simple as undo and remig/seed

---
+++ db/migrations/20210923054512-addColumnVotes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn(
+                'Votes',
+                'up',
+                Sequelize.BOOLEAN
+            );
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Votes');
+    }
+};
