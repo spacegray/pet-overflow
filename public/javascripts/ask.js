220f3d8d77e1eac34af6ad3b2d478b93d54cedbe
valeriareynososf <valeriayearup@gmail.com>
2021-09-23 11:53:47 -0700

    added button to popup modal instead of new page

---
+++ public/javascripts/ask.js
@@ -0,0 +1,13 @@
+document.querySelector(".askForm").addEventListener("click", () => {
+//      window.open(
+//     "/questions/ask",
+//     "formpopup",
+//     "width=400,height=400,resizeable,scrollbars"
+//   )
+document.querySelector(".modal").style.display = "block";
+})
+
+// function target_popup(ask) {
+//  ;
+//   form.target = "formpopup";
+// }
