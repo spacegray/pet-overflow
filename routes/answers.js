bbd1d57090e1457fea16ac7e44402d7ee1758fa4
9ziggy9 <davidarogers@protonmail.com>
2021-09-24 04:56:09 -0700

    answer validation added

--- routes/answers.js
+++ routes/answers.js
@@ -6,36 +6,49 @@ const { csrfProtection, asyncHandler } = require("./utils");
 const answer = require("../db/models/answer");
 const { Answer } = db;
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const answerValidators = [
+    check("content")
+    .exists({
+        checkFalsy: true
+    })
+    .withMessage("You need to type something, jabroni.")
+];
 
 router.post(
-  "/:id(\\d+)",
-  asyncHandler(async (req, res) => {
-    const { content } = req.body;
-    const { userId } = req.session.auth;
-    const questionId = parseInt(req.params.id, 10);
-    const votes = 0;
-    const views = 0;
+    "/:id(\\d+)",
+    answerValidators,
+    asyncHandler(async (req, res) => {
+        const { content } = req.body;
+        const { userId } = req.session.auth;
+        const questionId = parseInt(req.params.id, 10);
+        const question = await db.Question.findByPk(questionId);
+        const answers = await Answer.findAll({ where: { questionId } });
 
-    await Answer.create({
-      questionId,
-      content,
-      userId,
-      votes,
-      views,
-    });
-    res.redirect(`/questions/${questionId}`);
-  })
+        const validatorErrors = validationResult(req);
+
+        if (validatorErrors.isEmpty()) {
+            const votes = 0;
+            const views = 0;
+            await Answer.create({
+                questionId,
+                content,
+                userId,
+                votes,
+                views,
+            });
+            return req.session.save(() => res.redirect(`/questions/${questionId}`));
+        }
+
+        const errors = validatorErrors.array().map((error) => error.msg);
+        res.render('questionsContent', {
+            errors,
+            question,
+            answers,
+            questionId
+        });
+    })
 );
-
-// router.get(
-//   "/:id(\\d+)/answer",
-//   asyncHandler(async (req, res) => {
-//     const questionId = parseInt(req.params.id, 10);
-//     res.render("answer-form", { questionId });
-//   })
-// );
-
-// const questions = await db.Question.findAll();
-// res.render("questions", { questions });
 
 module.exports = router;
