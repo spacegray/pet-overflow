b9d863339e02c164934e8ff22d3b848d14abacd9
valeriareynososf <valeriayearup@gmail.com>
2021-12-27 00:21:02 -0800

    click out of modal by clicking outside

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -3,33 +3,6 @@ document.querySelector("#aboutUs").addEventListener("click", (e) => {
     document.querySelector(".modalAbout").style.display = "flex";
      document.querySelector("body").style.backgroundColor =
        "rgba(0, 0, 0, 0.5)";
-          // if (
-          //   document.querySelector(
-          //     ".circle" ||
-          //       ".dogs" ||
-          //       ".want" ||
-          //       ".vals_ask" ||
-          //       ".paw" ||
-          //       ".main_text" ||
-          //       ".box1" ||
-          //       ".box2" ||
-          //       ".box3" ||
-          //       ".box4" ||
-          //       ".q_button"
-          //   ) !== null
-          // ) {
-          //   document.querySelector(".circle").style.display = "none";
-          //   document.querySelector(".dogs").style.display = "none";
-          //   document.querySelector(".want").style.display = "none";
-          //   document.querySelector(".vals_ask").style.display = "none";
-          //   document.querySelector(".paw").style.display = "none";
-          //   document.querySelector(".main_text").style.display = "none";
-          //   document.querySelector(".box1").style.display = "none";
-          //   document.querySelector(".box2").style.display = "none";
-          //   document.querySelector(".box3").style.display = "none";
-          //   document.querySelector(".box4").style.display = "none";
-          //   document.querySelector(".q_button").style.display = "none";
-          // }
 })
 const mainmodal = document.querySelector(".modal");
 const loginmodal = document.querySelector(".modalLoginMain");
